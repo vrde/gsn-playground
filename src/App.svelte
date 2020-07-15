18b87ef3260d84052883e657b0da1f545104b769
vrde <agranzot@gmail.com>
2020-07-15 17:17:44 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import ethers from 'ethers';
+  import RelayProvider from "@opengsn/gsn/dist/src/relayclient";
+
+	export let name;
+  const wallet = ethers.Wallet.createRandom();
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+  <p>{wallet.address}</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
